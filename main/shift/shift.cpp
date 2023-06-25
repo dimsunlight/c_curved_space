b26f2376e9de0e470b3855a5b4041f96b1faa530
dimsunlight <toler.webb@gmail.com>
2023-06-24 20:05:00 -0400

    Found out you can add points and vectors to get new point objects, which is not surprising. Also, face_locations are built out of first the face (f#) and then... i think a set of barycentric coordinates, but std won't print them out. Tomorrow, I uuse the face numbers from the face_locations of locate to identify whether a shifted point is in the original face, and then start writing the unfolding approach.

--- main/shift/shift.cpp
+++ main/shift/shift.cpp
@@ -17,62 +17,51 @@
 #include <fstream>
 #include <cmath>
 #include <ctime>
-
-
-//typedefs:
-typedef CGAL::Exact_predicates_inexact_constructions_kernel             Kernel;
-typedef CGAL::Exact_predicates_exact_constructions_kernel_with_sqrt     KernelWithSqrt;
-typedef Kernel::Point_3                                                 Point_3;
-typedef Kernel::Vector_3                                                Vector_3;
-typedef Kernel::Ray_3                                                   Ray_3;
+//types and names
+typedef CGAL::Exact_predicates_inexact_constructions_kernel             K;
+typedef K::FT                                                           FT;
+typedef K::Point_2                                                      Point_2;
+typedef K::Ray_2                                                        Ray_2;
+typedef K::Point_3                                                      Point_3;
+typedef K::Vector_3                                                     Vector_3;
+typedef K::Ray_3                                                        Ray_3;
 typedef CGAL::Surface_mesh<Point_3>                                     Triangle_mesh;
-typedef CGAL::Surface_mesh_shortest_path_traits<Kernel, Triangle_mesh>  Traits;
-typedef CGAL::Surface_mesh_shortest_path<Traits>                        Surface_mesh_shortest_path;
-typedef boost::graph_traits<Triangle_mesh>                              Graph_traits;
-typedef Graph_traits::vertex_iterator                                   vertex_iterator;
-typedef Graph_traits::face_iterator                                     face_iterator;
-typedef typename Surface_mesh_shortest_path::Barycentric_coordinates    Barycentric_coordinates;
-typedef typename Surface_mesh_shortest_path::Face_location              Face_location;
-typedef CGAL::AABB_face_graph_triangle_primitive<Triangle_mesh>         AABB_face_graph_primitive;
-typedef CGAL::AABB_traits<Kernel, AABB_face_graph_primitive>            AABB_face_graph_traits;
-typedef CGAL::AABB_tree<AABB_face_graph_traits>                         AABB_tree;
-
-//namespaces
+typedef typename boost::graph_traits<Triangle_mesh>::vertex_descriptor           vertex_descriptor;
+typedef typename boost::graph_traits<Triangle_mesh>::face_descriptor             face_descriptor;
 namespace CP = CGAL::parameters;
 namespace PMP = CGAL::Polygon_mesh_processing;
-
+typedef PMP::Barycentric_coordinates<FT>                                Barycentric_coordinates;
+typedef PMP::Face_location<Triangle_mesh, FT>                                    Face_location;
 
 //function definitions
-auto simpleShift(Triangle_mesh mesh,Point_3 pos, Vector_3 move) {
-  
-  return 0;
-}
-
-auto overEdge(Triangle_mesh mesh, point_3 pos, Vector_3 move) {
-  
-  return 0;
+auto overEdge(Triangle_mesh mesh, Point_3 pos, Vector_3 move) {
+  return Point_3(0.0,0.0,0.0);
 }
 
 auto shift(Triangle_mesh mesh, Point_3 pos, Vector_3 move) {
   //first: check if we will be in our out of triangle after naive shift
-  //then: do the simple shift if we will, unfold shift if we won't
-  
-  const Point_3 query(1.2, 7.4, 0);
-  Face_location query_location = PMP::locate(query, tm);
+  //then: do the simple shift if we will, unfold shift if we won't 
 
   Point_3 trialNewPos = pos + move;
+  Face_location oldPosLocation = PMP::locate(pos, mesh);
+  std::cout << oldPosLocation.first << std::endl;
 
-  
+  Face_location newPosLocation = PMP::locate(trialNewPos,mesh);
+  std::cout << newPosLocation.first << std::endl;
+
+  bool withinTriangle=true;
+
   if (withinTriangle) {
-    return simpleShift(mesh, pos, move);
+    //could also return bary coordinates; PMP::locate(trialNewPos,mesh)
+    return trialNewPos;
   }
 
-  if (outsideTriangle) {
+  if (!withinTriangle) {
     return overEdge(mesh, pos, move);
   }
 
   std::cout << "neither in or out of triangle...?" << std::endl;
-  return 0;
+  return Point_3(0.0,0.0,0.0);
 }
 
 
@@ -91,9 +80,10 @@ int main(int argc, char* argv[]) {
 
  Vector_3 forceDisplacement = Vector_3(-0.0145553, 0.0453073, 0.176461);
  Point_3 pointToMove = Point_3(3.51033, 1.9177, 0);
+ 
+
 
  Point_3 newPos = shift(tmesh, pointToMove, forceDisplacement);
- std::cout << newPos << std::endl;
 
  return 0;
 }
