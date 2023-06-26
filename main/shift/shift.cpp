29bcd553401b5819a79b1a146779c50644f0bd55
dimsunlight <toler.webb@gmail.com>
2023-06-26 11:00:45 -0400

    added unfolding process deconstruction for when shift goes over an edge!
    now to actually write it...

--- main/shift/shift.cpp
+++ main/shift/shift.cpp
@@ -17,6 +17,7 @@
 #include <fstream>
 #include <cmath>
 #include <ctime>
+#include <math.h>
 //types and names
 typedef CGAL::Exact_predicates_inexact_constructions_kernel             K;
 typedef K::FT                                                           FT;
@@ -26,17 +27,35 @@ typedef K::Point_3                                                      Point_3;
 typedef K::Vector_3                                                     Vector_3;
 typedef K::Ray_3                                                        Ray_3;
 typedef CGAL::Surface_mesh<Point_3>                                     Triangle_mesh;
-typedef typename boost::graph_traits<Triangle_mesh>::vertex_descriptor           vertex_descriptor;
-typedef typename boost::graph_traits<Triangle_mesh>::face_descriptor             face_descriptor;
+typedef typename boost::graph_traits<Triangle_mesh>::vertex_descriptor  vertex_descriptor;
+typedef typename boost::graph_traits<Triangle_mesh>::face_descriptor    face_descriptor;
 namespace CP = CGAL::parameters;
 namespace PMP = CGAL::Polygon_mesh_processing;
 typedef PMP::Barycentric_coordinates<FT>                                Barycentric_coordinates;
-typedef PMP::Face_location<Triangle_mesh, FT>                                    Face_location;
+typedef PMP::Face_location<Triangle_mesh, FT>                           Face_location;
 
 //function definitions
-auto overEdge(Triangle_mesh mesh, Point_3 pos, Vector_3 move) {
+auto overEdge(Triangle_mesh mesh, Face_location f1, Face_location f2, Point_3 pos, Vector_3 move) {
   Face_location oldPosLocation = PMP::locate(pos,mesh);
-  Vector_3 oldNormal = PMP::compute_face_normal(oldPosLocation.first,mesh);
+  //compute normals are unit normals, so we shouldn't need additional normalization
+  Vector_3 oldNormal = PMP::compute_face_normal(f1.first,mesh);
+  Vector_3 newNormal = PMP::compute_face_normal(f2.first,mesh);
+  //normals compute fine
+
+  FT oldDotNew = CGAL::scalar_product(oldNormal,newNormal);
+  double angle = acos(oldDotNew);
+  //angle within expectations 
+  if (angle > double 0.5) {
+    std::cout << "Angle over 0.5!" << std::endl;
+  }
+  //task deconstruction for unfolding --essentially very simple version of geodesic finder
+  //sharedEdge = getSharedEdge(f1,f2);
+  //vertexToRotate = getRotatedVertexCoordinates(f1, f2, sharedEdge);
+  //newVertexLocation = rotateVertexAboutSharedAxis(vertexToRotate, sharedEdge);
+  //tempFace = Face(f2.v1,f2.v2, newVertexLocation);
+  //baryCoordinatesInNewFace = getBaryCoordinates(tempFace,pos+move); //coordinates tempFace are the same as those in f2
+  //newXYZCoordinates = getXYZCoordinates(mesh,baryCoordinatesInNewFace,f2);
+  //and done. 
 
   return Point_3(0.0,0.0,0.0);
 }
@@ -47,23 +66,17 @@ auto shift(Triangle_mesh mesh, Point_3 pos, Vector_3 move) {
 
   Point_3 trialNewPos = pos + move;
   Face_location oldPosLocation = PMP::locate(pos, mesh);
-
-  std::cout << oldPosLocation.first << std::endl;
-  std::cout << oldPosLocation.second[1] << " " << oldPosLocation.second[2] << " "<<oldPosLocation.second[3] << std::endl;
-
   Face_location newPosLocation = PMP::locate(trialNewPos,mesh);
-  std::cout << newPosLocation.first << std::endl;
-  std::cout << newPosLocation.second[1] << " " << newPosLocation.second[2] << " "<< newPosLocation.second[3] << std::endl;
 
-  bool withinTriangle=true;
-
+  bool withinTriangle= oldPosLocation.first == newPosLocation.first;
+  
   if (withinTriangle) {
     //could also return bary coordinates; PMP::locate(trialNewPos,mesh)
     return trialNewPos;
   }
 
   if (!withinTriangle) {
-    return overEdge(mesh, pos, move);
+    return overEdge(mesh, oldPosLocation, newPosLocation, pos, move);
   }
 
   std::cout << "neither in or out of triangle...?" << std::endl;
@@ -75,7 +88,7 @@ auto shift(Triangle_mesh mesh, Point_3 pos, Vector_3 move) {
 int main(int argc, char* argv[]) {
  
  //get input mesh from command line argument
- const std::string filename = (argc > 1) ? argv[1] : CGAL::data_file_path("sims_project/torusrb30.off");
+ const std::string filename = (argc > 1) ? argv[1] : CGAL::data_file_path("sims_project/torusrb12.off");
  Triangle_mesh tmesh;
  if(!CGAL::IO::read_polygon_mesh(filename, tmesh) ||
    !CGAL::is_triangle_mesh(tmesh))
