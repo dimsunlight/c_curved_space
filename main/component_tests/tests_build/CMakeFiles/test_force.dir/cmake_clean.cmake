58ab0be7c6f01e78f675d7cbedf6a476cc080e50
dimsunlight <toler.webb@gmail.com>
2023-07-26 15:40:54 -0400

    updating force tests & adding some small profiling

---
+++ main/component_tests/tests_build/CMakeFiles/test_force.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_force.dir/test_force.cpp.o"
+  "test_force"
+  "test_force.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_force.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main/component_tests/tests_build/CMakeFiles/test_shift.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_shift.dir/test_shift.cpp.o"
+  "test_shift"
+  "test_shift.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_shift.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
