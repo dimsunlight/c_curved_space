2deafbe57ea6e064ac84a3b23d9b9d01f16278f3
dimsunlight <toler.webb@gmail.com>
2023-08-04 22:43:43 -0400

    Separated previous structure of new shift function into a "draft 1" file and updated structure -- where the type of rotation we do depends on how many shared elements are found between the two faces -- in the u_shift file. Noted inclusions include the "unfold" function, which handles all the base scripting related to unfolding, and the two sharedEdge and sharedVertex rotation protocols.

--- main/attempt_simulate/sim_new/sim_new_source/u_shift_draft_1.cpp
+++ main/attempt_simulate/sim_new/sim_new_source/u_shift_draft_1.cpp
@@ -62,6 +62,45 @@ int findIndex(Point_3 loc,std::vector<Point_3> vec) {
   return index;
 }  
 
+auto rotateAboutSharedAxis(Point_3 target, std::vector<Point_3> axis, double rotAngle) {
+  //assumes that the shared axis is represented by a shared edge, rather than a vertex
+  Vector_3 axisVector = normalizer(Vector_3(axis[0],axis[1]));
+  Point_3 shiftedTarget = target - Vector_3({0,0,0}, axis[0]);//can subtract vectors from points, but not points themselves
+  std::cout << "target shifted for rotation to " << shiftedTarget << std::endl;
+  double u1 = axisVector.x(), u2 = axisVector.y(), u3=axisVector.z();
+  
+  double c = cos(rotAngle), s = sin(rotAngle);
+  double C = 1-c;
+  //unfortunately, I believe I have to multiply element wise in C++
+  double Identity[3][3] = {{c,0.0,0.0},{0.0,c,0.0},{0.0,0.0,c}};
+  double crossMatrix[3][3] = {{0,-s*u3,s*u2},{s*u3,0,-s*u1},{-s*u2,s*u1,0}};
+  double tensorProduct[3][3] = {{C*u1*u1,C*u1*u2,C*u1*u3},{C*u2*u1,C*u2*u2,C*u2*u3},{C*u3*u1,C*u3*u2,C*u3*u3}};
+  
+  double rMatrix[3][3];
+  //this double loop to define the rotation matrix is probably a place where it'd be easy to speed things up
+  //with more c++ knowledge
+  for (std::size_t i = 0; i < 3; i++) {
+    for (std::size_t j = 0; j < 3; j++) {
+	    rMatrix[i][j] = Identity[i][j] + crossMatrix[i][j] + tensorProduct[i][j];
+    }
+  }
+  
+  double rotatedStorage[3];
+  double targetStorage[3] = {shiftedTarget.x(),shiftedTarget.y(),shiftedTarget.z()};
+  
+  for (std::size_t i = 0; i < 3; i++) {
+    double product = 0;
+    for (std::size_t j = 0; j < 3; j++) {
+      product += rMatrix[i][j]*targetStorage[j];
+    }
+    rotatedStorage[i] = product; 
+  }
+  Point_3 rotatedTarget = Point_3(rotatedStorage[0],rotatedStorage[1],rotatedStorage[2]);
+  
+  rotatedTarget = rotatedTarget + Vector_3({0,0,0}, axis[0]);
+
+  return rotatedTarget;
+}
 
 //function definitions
 auto getVertexPositions(Triangle_mesh mesh, Triangle_mesh::face_index fIndex) {
@@ -79,7 +118,7 @@ auto getVertexPositions(Triangle_mesh mesh, Triangle_mesh::face_index fIndex) {
   return vertices;
 }
 
-auto getSharedElements(std::vector<Point_3> vs1, std::vector<Point_3> vs2) {
+auto getSharedEdge(std::vector<Point_3> vs1, std::vector<Point_3> vs2) {
   //common element finder for shared edge; double for loop is slow, but
   //that's fine given guaranteed small size. We could use set intersection finder, 
   //but that needs a sorted vector and I can't sort Point_3 objects!
@@ -96,20 +135,18 @@ auto getSharedElements(std::vector<Point_3> vs1, std::vector<Point_3> vs2) {
   return sharedElements;
 }
 
-auto getUnsharedElements(std::vector<Point_3> vertices, std::vector<Point_3> shared) {
-  //find vertices not in "shared."  
-  //Shared is the vector of vertex locations
- 
-  std::vector<Point_3> oddElements;
+auto getVertexToRotate(std::vector<Point_3> vs2, std::vector<Point_3> sEdge) {
+  //assuming second face includes the vertex we want to rotate
+  std::vector<Point_3> oddVertex;
   
   for (Point_3 i: vs2) {
-    if (std::find(shared.begin(), shared.end(), i) != shared.end()) {}
+    if (std::find(sEdge.begin(), sEdge.end(), i) != sEdge.end()) {}
     else {
-      oddElements.push_back(i);
+      oddVertex.push_back(i);
     }
   }
 
-  return oddElements;
+  return oddVertex;
 }
 
 double angleBetween(Face_location f1, Face_location f2, mesh) {
@@ -127,64 +164,8 @@ double angleBetween(Face_location f1, Face_location f2, mesh) {
   return angle;
 }
 
-auto rotateAboutAxis(std::vector<Point_3> targets, std::vector<Point_3> axis, double rotAngle) {
-  //rotation about an axis named axis; handles rotation of multiple targets at once
-  
-  Vector_3 axisVector = normalizer(Vector_3(axis[0],axis[1]));
-  std::vector<Point_3> shiftedTargets; 
-
-  for (Point_3 target: targets) {
-    shiftedTargets.push_back(target - Vector_3({0,0,0}, axis[0]);
-  }
-  std::cout << "targets shifted for rotation to " << std::endl;
-  for (Point_3 shiftedTarget: shiftedTargets) std::cout << shiftedTarget << std::endl;
-
-  double u1 = axisVector.x(), u2 = axisVector.y(), u3=axisVector.z();
-  
-  double c = cos(rotAngle), s = sin(rotAngle);
-  double C = 1-c;
-  //unfortunately, I believe I have to multiply element wise in C++
-  double Identity[3][3] = {{c,0.0,0.0},{0.0,c,0.0},{0.0,0.0,c}};
-  double crossMatrix[3][3] = {{0,-s*u3,s*u2},{s*u3,0,-s*u1},{-s*u2,s*u1,0}};
-  double tensorProduct[3][3] = {{C*u1*u1,C*u1*u2,C*u1*u3},{C*u2*u1,C*u2*u2,C*u2*u3},{C*u3*u1,C*u3*u2,C*u3*u3}};
-  
-  double rMatrix[3][3];
-  //this double loop to define the rotation matrix is probably a place where it'd be easy to speed things up very slightly
-  //with more c++ knowledge
-  for (std::size_t i = 0; i < 3; i++) {
-    for (std::size_t j = 0; j < 3; j++) {
-	    rMatrix[i][j] = Identity[i][j] + crossMatrix[i][j] + tensorProduct[i][j];
-    }
-  }
-  
-  std::vector<Point_3> rotatedTargets;
-  double rotatedStorage[3];
-  double targetStorage[3];
-  
-  //rotate and shift back all targets
-  for (Point_3 shiftedTarget: shiftedTargets) {
-    targetStorage = {shiftedTarget.x(),shiftedTarget.y(),shiftedTarget.z()};
-  
-    for (std::size_t i = 0; i < 3; i++) {
-      double product = 0;
-      for (std::size_t j = 0; j < 3; j++) {
-        product += rMatrix[i][j]*targetStorage[j];
-      }
-      rotatedStorage[i] = product; 
-    }
-    Point_3 rotatedTarget = Point_3(rotatedStorage[0],rotatedStorage[1],rotatedStorage[2]);
-    
-    //undo shift at the end
-    rotatedTargets.push_back(rotatedTarget + Vector_3({0,0,0}, axis[0]));
-  }
-  return rotatedTargets;
-}
-
-
-
-auto verticesForUnfoldedFace(std::vector<Point_3> sharedEdge, std::vector<Point_3> vertices2, std::vector<Point_3> newLocations ) {
+auto verticesForUnfoldedFace(std::vector<Point_3> sharedEdge, std::vector<Point_3> vertices2 ) {
   //get original index locations -- fact that we always use second face is important here
-  //REWRITE TO HANDLE NEW LOCATIONS BEING LARGER THAN SIZE 1
   int ind1 = findIndex(sharedEdge[0],vertices2);
   int ind2 = findIndex(sharedEdge[1],vertices2); 
   //then, index of rotated vertex will be whichever of these is not included, so we create 
@@ -204,7 +185,7 @@ auto verticesForUnfoldedFace(std::vector<Point_3> sharedEdge, std::vector<Point_
   std::vector<Point_3> tempTrio = {0,0,0};
   tempTrio[ind1] = sharedEdge[0];
   tempTrio[ind2] = sharedEdge[1];
-  tempTrio[rIndex] = newLocations[0];
+  tempTrio[rIndex] = newVertexLocation;
 
   return tempTrio;
 }
@@ -227,63 +208,95 @@ Point_3 getNewXYZLocation(Point_3 flatLocation, Triangle_mesh tempMesh, Face_loc
 
 }
 
-
-
-auto sharedEdge(Triangle_mesh mesh, Face_location f1, Face_location f2, Point_3 pos, Vector_3 move,
-                std::vector<Point_3> vertices1, std::vector<Point_3> vertices2, std::vector<Point_3> shared) {
+auto overEdge(Triangle_mesh mesh, Face_location f1, Face_location f2, Point_3 pos, Vector_3 move) {
   //functionality to move a particle location when it must travel over a mesh edge
   
+  //first: grab original location via locate for use later
+  Face_location oldPosLocation = PMP::locate(pos,mesh);
+
   //get angle between faces we need to move between
   double faceAngle = angleBetween(f1,f2,mesh);
   
-  std::vector<Point_3> vertexToRotate = getUnsharedElements(vertices2, shared);
-
-  Point_3 newVertexLocation = rotateAboutAxis(vertexToRotate[0], shared, -angle);
+  //grab the positions of the vertices for the two faces
+  std::vector<Point_3> vertices1 = getVertexPositions(mesh, f1.first), vertices2 = getVertexPositions(mesh, f2.first);
   
-  std::vector<Point_3> tempTrio = verticesForUnfoldedFace(sharedEdge, vertices2, newVertexLocation);
+  std::vector<Point_3> sharedEdge = getSharedEdge(vertices1,vertices2); 
+  
+  std::vector<Point_3> vertexToRotate = getVertexToRotate(vertices2, sharedEdge);
+  //vertexToRotate returns a vector (in the case of a shared vertex but not a shared edge), but i haven't 
+  //implemented behavior for that -- so for now we just grab the ``first'' element instead of splitting up
+  //which functions we use in the following
+  Point_3 newVertexLocation = rotateAboutSharedAxis(vertexToRotate[0], sharedEdge, -angle);
+  
+  std::vector<Point_3> tempTrio = verticesForUnfoldedFace(sharedEdge, vertices2);
 
   Triangle_mesh tempMesh = createTemporaryMesh(tempTrio);
 
   return getNewXYZLocation(pos+move,tempMesh, f2);
 }
 
-auto sharedVertex(Triangle_mesh mesh, Face_location f1, Face_location f2, Point_3 pos, Vector_3 move,
-                std::vector<Point_3> vertices1, std::vector<Point_3> vertices2, std::vector<Point_3> shared) {
-  //functionality to unfold if we must travel through a vertex into a face that only shares that vertex 
-  
-  //get angle between faces we need to move between
-  double faceAngle = angleBetween(f1,f2,mesh);
-  
-  std::vector<Point_3> verticesToRotate = getUnsharedElements(vertices2,shared); //two vertices to rotate from new face...
-  
-  std::vector<Point_3> unsharedEdge = getUnsharedElements(vertices1,shared); //are rotated around the axis of the two unshared vertices from the original. 
+Point_3 shift(Triangle_mesh mesh, Point_3 pos, Vector_3 move) {
+  //first: check if we will be in our out of triangle after naive shift
+  //then: do the simple shift if we will, unfold shift if we won't 
 
-  std::vector<Point_3> newVertexLocations = rotateAboutAxis(verticesToRotate, unsharedEdge, angle); 
-  
-  !!!std::vector<Point_3> tempTrio = verticesForUnfoldedFace(shared, vertices2, newVertexLocations)!!!;
-  
-  Triangle_mesh tempMesh = createTemporaryMesh(tempTrio); 
- 
-  return getNewXYZLocation(pos+move, tempMesh, f2); 
+  Face_location oldPosLocation = PMP::locate(pos, mesh);
+  Point_3 meshPos = PMP::construct_point(oldPosLocation, mesh);
+  Point_3 trialNewPos = meshPos + move;
+  Face_location newPosLocation = PMP::locate(trialNewPos,mesh);
 
+  bool withinTriangle= oldPosLocation.first == newPosLocation.first;
+  
+  if (withinTriangle) {
+    //could also return bary coordinates; PMP::locate(trialNewPos,mesh)
+    return trialNewPos;
+  }
+
+  if (!withinTriangle) {
+    return overEdge(mesh, oldPosLocation, newPosLocation, meshPos, move);
+  }
+
+  std::cout << "neither in or out of triangle...?" << std::endl;
+  return Point_3(0.0,0.0,0.0);
 }
 
-auto unfold(Triangle_mesh mesh, Face_loation f1, Face_location f2, Point_3 pos, Vector_3 move) {
-  //folding function that calls either sharedEdge or sharedVertex depending on the output of getSharedElements
-
-  //grab the positions of the vertices for the two faces
+std::vector<Point_3> sharedEdgeUnfolding(face1,face2,mesh) {
+  //already have all the functionality for this in overEdge, just need to execute. 
+  double faceAngle = angleBetween(face1,face2,mesh);
   std::vector<Point_3> vertices1 = getVertexPositions(mesh, f1.first), vertices2 = getVertexPositions(mesh, f2.first);
-  
-  std::vector<Point_3> sharedElements = getSharedElements(vertices1,vertices2); 
-  
-  if (sharedElements.size() == 2) {
-    return sharedEdge(mesh, f1, f2, pos, move, vertices1, vertices2, sharedElements); 
-  }
-  
-  if (sharedElements.size() == 1) {
-    return sharedVertex(mesh, f1, f2, pos, move, vertices1, vertices2, sharedElements); 
-  }
+  std::vector<Point_3> sharedEdge = getSharedEdge(vertices1,vertices2);
+
+  std::vector<Point_3> vertexToRotate = getVertexToRotate(vertices2,sharedEdge);
+
+  Point_3 rotatedVertexLocation = rotateAboutSharedAxis(vertexToRotate[0],sharedEdge,-angle);
+
+  return verticesForUnfoldedFace(sharedEdge,vertices2);
 }
+
+Vector_3 reduceMove(Vector_3 move, double reduceBy) {
+  //utility function for "new" shift function
+  //reduces length of a vector by an amountreduceBy
+  Vector_3 unitLength = normalize(move); 
+  double   magnitude  = vectorMagnitude(move);
+  double   newMagnitude = magnitude-reduceBy;
+  return unitLength*newMagnitude;
+}
+
+//functions needing definitions
+
+
+
+std::vector<Point_3> singleSharedVertexUnfolding(Face_descriptor f, Point_3 vertex);
+
+Face_descriptor sharedFaces(Point_3 vertex, Triangle_mesh::face_index findex, Triangle_mesh mesh) {
+  //we want to find what face we're going into when traveling through a vertex
+  //to that end -- determine what face the path most directly goes toward...
+  //come to think of it, faces that share a vertex are not unique, so we've got a 
+  //small collection of problems to solve here. 
+   
+  
+}
+
+
 
 
 Point_3 shift_n(Triangle_mesh mesh, Point_3 pos, Vector_3 move) {
@@ -305,17 +318,31 @@ Point_3 shift_n(Triangle_mesh mesh, Point_3 pos, Vector_3 move) {
 
   //useful items for loop w/definition
   bool intersection = true; // true until we have checked all the edges/vertex and verified there's no intersection
-  Segment_3 checkSegment = Segment_3(pos, pos+move); //now we define segment to be checked against... redefine later
+  Segment_3 checkSegment = Segment_3(pos, pos+move); //now we define segment to be checked against... neat redefinitions later
   Triangle_mesh unfoldedMesh = createTemporaryMesh(vertexList);
   Face_descriptor currentFace = unfoldedMesh[0]; //should be only face of unfoldedMesh for now
 
+
   while(intersection){
+    vertexList = getVertices(oldPosLocation.first);
     edgesList  = createEdgeSegments(vertexList);
     skip = false;
+    for (Point_3 vert: vertexList) {
+      auto result = intersection(checkSegment,vert); //give point of intersection if there is one   
+      if (result) {
+        connectedFace = sharedFaces(vertex, oldPosLocation.first, mesh);
+        std::vector<Point_3> ssvu = singleSharedVertexUnfolding(connectedFace);
+        currentFace = unfoldedMesh.add_face(ssvu[0],ssvu[1],ssvu[2]) //need to make sure these are in the original order!!!;
+        move = reduceMove(move,lengthToSharedElement); //decrease move size by length to intersected vertex/edge. idk how we're getting that yet
+        checkSegment = Segment_3(intersection_point, intersection_point+move);
+        skip = true; 
+        continue; // skip the rest of the for & while loop once we've found an intersection, if possible
+      }
+    }
+    if (skip) continue;
 
     for (Segment_3 edge: edgesList) {
       auto result = intersection(checkSegment,vert);
-
       if (intersects(checkSegment,edge) {
         connectedFace = sharedFace(edge, oldPosLocation.first, original_mesh_faces);
         std::vector<Point_3> seu = sharedEdgeUnfolding(currentFace,connectedFace,mesh); //mostly does what overEdge currently does
@@ -326,7 +353,7 @@ Point_3 shift_n(Triangle_mesh mesh, Point_3 pos, Vector_3 move) {
         continue; // skip the rest of the for & while loop once we've found an intersection, if possible
       }
     } 
-    if (skip) continue; //just avoiding last statement
+    if (skip) continue;
     intersection = false; //if we haven't found an intersection, there is no intersection. 
   }
 }
